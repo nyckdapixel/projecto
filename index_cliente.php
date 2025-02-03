3558267ef1201787624fb0469742b116136f12e4
nyckdapixel <nyck2305cintra@gmail.com>
2025-02-03 18:54:37 -0400

    Update index_cliente.php

--- index_cliente.php
+++ index_cliente.php
@@ -3,8 +3,9 @@ include 'conexao.php';
 
 if (!isset($_GET['id'])) {
     header("Location: clientes.php");
-    exit();
+   
 }
+
 
 $cliente_id = $_GET['id'];
 $cliente = $conn->query("SELECT * FROM clientes WHERE id = $cliente_id")->fetch_assoc();
@@ -15,7 +16,8 @@ $cliente = $conn->query("SELECT * FROM clientes WHERE id = $cliente_id")->fetch_
 <head>
     <meta charset="UTF-8">
     <title>Painel do Cliente</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css"> <!-- Adicionando o CSS externo -->
 </head>
 <body>
 
